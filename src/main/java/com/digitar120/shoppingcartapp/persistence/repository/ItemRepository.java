c08b200595867c3ae34b771d55919f3003620186
digitar120 <digitar120@outlook.com>
2023-08-21 19:37:44 -0300

    Agregar método de búsqueda por descripción

--- src/main/java/com/digitar120/shoppingcartapp/persistence/repository/ItemRepository.java
+++ src/main/java/com/digitar120/shoppingcartapp/persistence/repository/ItemRepository.java
@@ -3,5 +3,8 @@ package com.digitar120.shoppingcartapp.persistence.repository;
 import com.digitar120.shoppingcartapp.persistence.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    public Item findByDescription(String description);
 }
