5c328767faddbbbd57d8264c8e5b196554347709
digitar120 <digitar120@outlook.com>
2023-08-21 15:14:44 -0300

    Agregar archivos básicos del proyecto

---
+++ src/test/java/com/digitar120/shoppingcartapp/ShoppingCartAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitar120.shoppingcartapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
