6fc390097b7c6db227153197ba5f5f795e8fc543
digitar120 <digitar120@outlook.com>
2023-08-23 17:33:19 -0300

    Agregar caso para probar el guardado al repo

--- src/test/java/com/digitar120/shoppingcartapp/service/ItemServiceTest.java
+++ src/test/java/com/digitar120/shoppingcartapp/service/ItemServiceTest.java
@@ -21,6 +21,7 @@ public class ItemServiceTest {
 
     public static final String LAPIZ = "Lápiz";
     public static final String LAPICERA = "Lapicera";
+    public static final long ITEM_1 = 1L;
 
     @InjectMocks
     private ItemService itemService;
@@ -32,11 +33,13 @@ public class ItemServiceTest {
     @DisplayName("Test findAll not empty OK")
     public void test_when_findAll_then_listNotEmpty(){
         // Mock del repositorio
-        Item item = new Item(1L, LAPIZ, 1);
+        Item item = new Item(ITEM_1, LAPIZ, 1);
         Item item2 = new Item(2L, LAPICERA, 2);
+
         List<Item> list = new ArrayList<>();
         list.add(item);
         list.add(item2);
+
         when(repository.findAll()).thenReturn(list);
 
         // Ejecución de la llamada
@@ -51,7 +54,7 @@ public class ItemServiceTest {
     @DisplayName("Test findAll returns given list OK")
     public void test_when_findAll_then_listEqualsGivenList(){
         // Mock del repositorio
-        Item item = new Item(1L, LAPIZ, 1);
+        Item item = new Item(ITEM_1, LAPIZ, 1);
         Item item2 = new Item(2L, LAPICERA, 2);
         List<Item> list = new ArrayList<>();
         list.add(item);
@@ -69,7 +72,7 @@ public class ItemServiceTest {
     @DisplayName("Test findByDescription OK")
     public void test_when_findByDescription_then_returnsCorrectItem(){
         // Mock del repositorio
-        Item item = new Item(1L, LAPIZ, 1);
+        Item item = new Item(ITEM_1, LAPIZ, 1);
 
         when(repository.findByDescription(LAPIZ)).thenReturn(item);
 
@@ -78,6 +81,20 @@ public class ItemServiceTest {
 
         // Asserts
         assertEquals(LAPIZ, returned_item.getDescription());
+    }
+
+    @Test
+    @DisplayName("Test saveToRepo saves ITEM_1")
+    public void test_when_saveToRepo_then_repoSavesITEM_1(){
+        // Arrange
+        Item item = new Item(0L, LAPIZ, 1);
+
+        // Act
+        when(repository.save(item)).thenReturn(item);
+        Item actualResult = itemService.saveToRepo(item);
+
+        // Assert
+        assertEquals(item, actualResult);
     }
 
 }
