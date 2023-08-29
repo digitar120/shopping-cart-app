1aca9d4d47d07f12ad845420983e89cc10e5e346
digitar120 <digitar120@outlook.com>
2023-08-29 14:54:34 -0300

    Agregar anotación necesaria

--- src/main/java/com/digitar120/shoppingcartapp/mapper/ItemToEditedItem.java
+++ src/main/java/com/digitar120/shoppingcartapp/mapper/ItemToEditedItem.java
@@ -2,7 +2,9 @@ package com.digitar120.shoppingcartapp.mapper;
 
 import com.digitar120.shoppingcartapp.persistence.entity.Item;
 import com.digitar120.shoppingcartapp.service.dto.EditedItemDTO;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ItemToEditedItem implements IMapper<EditedItemDTO, Item>{
 
     @Override
