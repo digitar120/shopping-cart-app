011aa512e80d8d314e3b1fc045d7a0766a74466a
digitar120 <digitar120@outlook.com>
2023-09-18 21:10:11 -0300

    Agregar método para crear un carrito

--- src/main/java/com/digitar120/shoppingcartapp/controller/CartController.java
+++ src/main/java/com/digitar120/shoppingcartapp/controller/CartController.java
@@ -3,6 +3,7 @@ package com.digitar120.shoppingcartapp.controller;
 import com.digitar120.shoppingcartapp.persistence.entity.Cart;
 import com.digitar120.shoppingcartapp.persistence.entity.Item;
 import com.digitar120.shoppingcartapp.service.CartService;
+import com.digitar120.shoppingcartapp.service.dto.NewCartDTO;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,19 +18,32 @@ public class CartController {
         this.service = service;
     }
 
+    // Listar todos
     @GetMapping
     public List<Cart> findAll(){
         return this.service.findAll();
     }
 
+    // Encontrar por id
     @GetMapping("/{id}")
     public Cart findById(@PathVariable Long id){
         return service.findById(id);
     }
 
+    // Listar contenidos por id
     @GetMapping("/{id}/items")
     public Set<Item> getCartItems (@PathVariable Long id){
         return service.getContent(id);
     }
+
+    // Crear nuevo
+    @PostMapping
+    public Cart newCart(@RequestBody NewCartDTO newCartDTO){
+        return service.newCart(newCartDTO);
+    }
+
+    // Agregar elementos
+    // Eliminar elementos
+    // Eliminar carrito
 
 }
