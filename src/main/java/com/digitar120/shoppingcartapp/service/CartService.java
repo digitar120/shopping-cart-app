8c03b18422acd760e054ac4f8e3f2294e1d90a26
digitar120 <digitar120@outlook.com>
2023-09-19 21:16:41 -0300

    Eliminar uso de servicios ajenos, refactorizar, y agregar métodos para eliminar elementos y eliminar carritos

--- src/main/java/com/digitar120/shoppingcartapp/service/CartService.java
+++ src/main/java/com/digitar120/shoppingcartapp/service/CartService.java
@@ -6,7 +6,6 @@ import com.digitar120.shoppingcartapp.persistence.entity.Cart;
 import com.digitar120.shoppingcartapp.persistence.entity.Item;
 import com.digitar120.shoppingcartapp.persistence.entity.Product;
 import com.digitar120.shoppingcartapp.persistence.repository.CartRepository;
-import com.digitar120.shoppingcartapp.persistence.repository.ItemRepository;
 import com.digitar120.shoppingcartapp.service.dto.NewCartDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -19,61 +18,76 @@ import java.util.Set;
 @Service
 public class CartService {
 
-    private final CartRepository cartRepository;
-    private final ItemRepository itemRepository;
-    private final ProductService productService;
+    private final CartRepository repository;
     private final CartDTOtoCart mapper;
 
-    public CartService(CartRepository cartRepository, ItemRepository itemRepository, ProductService productService, CartDTOtoCart mapper) {
-        this.cartRepository = cartRepository;
-        this.itemRepository = itemRepository;
-        this.productService = productService;
+    public CartService(CartRepository repository, CartDTOtoCart mapper) {
+        this.repository = repository;
         this.mapper = mapper;
     }
 
 
     // Listar carritos
     public List<Cart> findAll() {
-        return cartRepository.findAll();
+        return repository.findAll();
     }
 
     // Encontrar carrito mediante ID
     public Cart findById(Long id){
-        return cartRepository.findById(id).orElseThrow( () -> new MyException("No se encontró el carrito con ID "+ id, HttpStatus.NOT_FOUND));
+        return repository.findById(id).orElseThrow( () -> new MyException("No se encontró el carrito con ID "+ id, HttpStatus.NOT_FOUND));
     }
 
     // Listar contenidos de un carrito
     @Transactional
     public Set<Item> getContent(Long id){
-        Optional<Cart> optionalCart = cartRepository.findById(id);
+        Optional<Cart> optionalCart = repository.findById(id);
         if (optionalCart.isEmpty()){
             throw new MyException("No se encontró el carrito con ID "+ id, HttpStatus.NOT_FOUND);
         } else {
-            return cartRepository.findById(id).get().getItems();
+            return repository.findById(id).get().getItems();
         }
     }
 
     // Crear nuevo carrito
     public Cart newCart(NewCartDTO cartDTO){
         Cart newCart = mapper.map(cartDTO);
-        return cartRepository.save(newCart);
+        return repository.save(newCart);
+    }
+
+    // Agregar elementos a un carrito
+    // Trabajar el tema alterar la cantidad de un item
+    public Cart addItemToCart(Long cartId, Long productId, Integer quantity){
+        Cart cart = this.findById(cartId);
+
+        cart.getItems().add(new Item(
+                quantity,
+                cart,
+                new Product(productId)
+        ));
+
+        return repository.save(cart);
+
+    }
+
+    // Eliminar elementos de un carrito
+    public Cart deleteItemFromCart(Long cartId, Long itemId){
+        Cart cart = repository.findById(cartId).get();
+        Item matchedItem = new Item();
+
+        for (Item element: cart.getItems()){
+            if (element.getId().equals(itemId)){
+                matchedItem = element;
+            }
+        }
+
+        cart.getItems().remove(matchedItem);
+
+        return repository.save(cart);
+
     }
 
     // Eliminar un carrito
     public void deleteCart(Long id){
-        cartRepository.deleteById(id);
+        repository.deleteById(id);
     }
-
-    // Agregar elementos a un carrito
-    /*
-    public Item addItemToCart(Long productId, Long cartId, Integer quantity){
-        Product product = productService.findById(productId);
-        Cart cart = this.findById(cartId);
-
-        Item item = new Item (quantity, cart, product);
-
-
-    }*/
-
-    // Eliminar elementos de un carrito
 }
