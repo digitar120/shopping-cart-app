f607866eb0c7c7a415d941a9b7e3304f187e8d39
digitar120 <digitar120@outlook.com>
2023-09-25 17:30:59 -0300

    Agregar verificaciones a métodos; Correcciones menores

--- src/main/java/com/digitar120/shoppingcartapp/service/CartService.java
+++ src/main/java/com/digitar120/shoppingcartapp/service/CartService.java
@@ -38,13 +38,12 @@ public class CartService {
     }
 
     // Listar contenidos de un carrito
-    @Transactional
     public Set<Item> getContent(Long id){
         Optional<Cart> optionalCart = repository.findById(id);
         if (optionalCart.isEmpty()){
             throw new MyException("No se encontró el carrito con ID "+ id, HttpStatus.NOT_FOUND);
         } else {
-            return repository.findById(id).get().getItems();
+            return optionalCart.get().getItems();
         }
     }
 
@@ -56,38 +55,97 @@ public class CartService {
 
     // Agregar elementos a un carrito
     // Trabajar el tema alterar la cantidad de un item
+    @Transactional
     public Cart addItemToCart(Long cartId, Long productId, Integer quantity){
-        Cart cart = this.findById(cartId);
 
-        cart.getItems().add(new Item(
-                quantity,
-                cart,
-                new Product(productId)
-        ));
+        // Verificar que el carrito especificado existe
+        Optional<Cart> optionalCart = repository.findById(cartId);
+        if (optionalCart.isEmpty()){
+            throw new MyException("No se encontró el carrito N°" + cartId, HttpStatus.NOT_FOUND);
+        } else {
 
-        return repository.save(cart);
+            // Verificar que el valor de cantidad sea válido
+            if(quantity <= 0){
+                throw new MyException("La cantidad ingresada es inválida.", HttpStatus.BAD_REQUEST);
+            } else {
+                Cart cart = this.findById(cartId);
 
+                // Existe un ítem con el mismo referencedProduct?
+                Item matchedItem = new Item();
+                Product matchedProduct = new Product();
+
+                for (Item element: cart.getItems()){
+                    if (element.getReferencedProduct().getId().equals(productId)){
+                        matchedItem = element;
+                        matchedProduct = element.getReferencedProduct();
+                    }
+                }
+                // Si existe, igualar su cantidad a la ingresada
+                if (matchedProduct.getId() != null){
+                    // Eliminar el ítem encontrado
+                    cart.getItems().remove(matchedItem);
+
+                    // Editar y agregar nuevamente el ítem.
+                    matchedItem.setQuantity(quantity);
+                    cart.getItems().add(matchedItem);
+
+                    return repository.save(cart);
+                }
+                // Si no existe, construir el ítem y agregarlo
+                else {
+                    // Cómo verificar si un producto existe o no? Debería agregar el servicio o el repositorio de
+                    // Product? En el mientras tanto, pruebo una solución más ambigua.
+                    try {
+                        cart.getItems().add(new Item(
+                                quantity,
+                                cart,
+                                new Product(productId)
+                        ));
+                        return repository.save(cart);
+                    } catch (Exception e){
+                        throw new MyException("No se encontró el producto N°" + productId,HttpStatus.NOT_FOUND);
+                    }
+                }
+            }
+        }
     }
 
     // Eliminar elementos de un carrito
     public Cart deleteItemFromCart(Long cartId, Long itemId){
-        Cart cart = repository.findById(cartId).get();
-        Item matchedItem = new Item();
 
-        for (Item element: cart.getItems()){
-            if (element.getId().equals(itemId)){
-                matchedItem = element;
+        // Verificar que el carrito especificado existe
+        Optional<Cart> optionalCart = repository.findById(cartId);
+        if (optionalCart.isEmpty()){
+            throw new MyException("No se encontró el carrito N°" + cartId, HttpStatus.NOT_FOUND);
+        } else {
+            Cart cart = optionalCart.get();
+
+            // Verificar que el ítem ingresado existe
+            Item matchedItem = new Item();
+
+            for (Item element: cart.getItems()){
+                if (element.getId().equals(itemId)){
+                    matchedItem = element;
+                }
+            }
+
+            if (matchedItem.getId() == null){
+                throw new MyException("No se encontró el ítem N°" + itemId, HttpStatus.NOT_FOUND);
+            } else {
+                cart.getItems().remove(matchedItem);
+                return repository.save(cart);
             }
         }
-
-        cart.getItems().remove(matchedItem);
-
-        return repository.save(cart);
 
     }
 
     // Eliminar un carrito
     public void deleteCart(Long id){
-        repository.deleteById(id);
+        Optional<Cart> optionalCart = repository.findById(id);
+        if (optionalCart.isEmpty()){
+            throw new MyException("No se encontró el carrito N°" + id, HttpStatus.NOT_FOUND);
+        } else {
+            repository.deleteById(id);
+        }
     }
 }
