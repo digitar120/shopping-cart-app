fa4848409894ea688375a2fefd7eb7139b4a1b24
digitar120 <digitar120@outlook.com>
2023-09-29 14:32:22 -0300

    Agregar constructor con ID

--- src/main/java/com/digitar120/shoppingcartapp/persistence/entity/Cart.java
+++ src/main/java/com/digitar120/shoppingcartapp/persistence/entity/Cart.java
@@ -33,4 +33,8 @@ public class Cart {
         // orphanremoval: al eliminar un ítem desde el controlador de carritos, se devuelve un resultado correcto pero
         // no se propaga el resultado a la base de datos.
     private Set<Item> items = new HashSet<>();
+
+    public Cart(Long id){
+        this.id = id;
+    }
 }
