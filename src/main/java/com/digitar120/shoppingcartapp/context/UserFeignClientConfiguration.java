7665428c4af40e256e2d4d2824a96e667d5c556a
Gabriel <digitar120@outlook.com>
2025-03-24 18:23:36 -0300

    feat: Remove security configurations

--- src/main/java/com/digitar120/shoppingcartapp/context/UserFeignClientConfiguration.java
+++ src/main/java/com/digitar120/shoppingcartapp/context/UserFeignClientConfiguration.java
@@ -18,51 +18,13 @@ import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedCli
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 
 /**
- * Outgoing authorized FeignClient calls configuration. Defines call behavior for calls to the User service.
+ * Outgoing FeignClient calls configuration. Defines call behavior for calls to the User service.
  * @author Gabriel Pérez (digitar120)
  * @see com.digitar120.shoppingcartapp.service.CartService#findByUserId(Integer) 
  */
 
 @Configuration
 public class UserFeignClientConfiguration {
-
-    @Bean
-    public OAuth2AuthorizedClientManager authorizedClientManager(
-            ClientRegistrationRepository clientRegistrationRepository,
-            OAuth2AuthorizedClientRepository authorizedClientRepository) {
-
-        OAuth2AuthorizedClientProvider authorizedClientProvider =
-                OAuth2AuthorizedClientProviderBuilder.builder()
-                        .authorizationCode()
-                        .refreshToken()
-                        .clientCredentials()
-                        .build();
-
-        DefaultOAuth2AuthorizedClientManager authorizedClientManager =
-                new DefaultOAuth2AuthorizedClientManager(
-                        clientRegistrationRepository, authorizedClientRepository);
-        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
-
-        return authorizedClientManager;
-    }
-
-    /**
-     * This bean adds a header containing an OAuth2 token to the HTTP request. The access token is preconfigured.
-     * @param manager
-     * @return
-     */
-    @Bean
-    public RequestInterceptor oauth2FeignRequestInterceptor(OAuth2AuthorizedClientManager manager){
-        return requestTemplate -> {
-            OAuth2AuthorizeRequest auth2AuthorizeRequest = OAuth2AuthorizeRequest
-                    .withClientRegistrationId("keycloak")
-                    .principal("principal")
-                    .build();
-
-            OAuth2AuthorizedClient client = manager.authorize(auth2AuthorizeRequest);
-            requestTemplate.header("Authorization", "Bearer " + client.getAccessToken().getTokenValue());
-        };
-    }
 
     /**
      * Part of the circuit breaker pattern applied to outgoing calls.
