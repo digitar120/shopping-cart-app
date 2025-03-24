706ddba0123ed3dc3f7b9c945d6431df15702d71
Gabriel <digitar120@outlook.com>
2025-03-24 19:31:09 -0300

    fix: Remove security artifacts from the POM file, and imports referring to them

--- src/main/java/com/digitar120/shoppingcartapp/context/UserFeignClientConfiguration.java
+++ src/main/java/com/digitar120/shoppingcartapp/context/UserFeignClientConfiguration.java
@@ -8,14 +8,6 @@ import lombok.RequiredArgsConstructor;
 import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.*;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 
 /**
  * Outgoing FeignClient calls configuration. Defines call behavior for calls to the User service.
