f8ea7482b8fc1cfeb61d72740bd157e1500575e2
Gabriel <digitar120@outlook.com>
2025-05-28 15:44:00 -0300

    fix: Fix controller using incorrect Swagger annotations for api-docs

--- src/main/java/com/digitar120/shoppingcartapp/controller/CartController.java
+++ src/main/java/com/digitar120/shoppingcartapp/controller/CartController.java
@@ -4,6 +4,7 @@ import com.digitar120.shoppingcartapp.persistence.entity.Cart;
 import com.digitar120.shoppingcartapp.persistence.entity.Item;
 import com.digitar120.shoppingcartapp.service.CartService;
 import com.digitar120.shoppingcartapp.service.dto.NewCartDTO;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.v3.oas.annotations.Operation;
@@ -34,7 +35,7 @@ public class CartController {
      * List all entries.
      * @return A cart list of all entries in the database.
      */
-    @Operation(summary = "List all carts.", description = "Produces a JSON object containing all registered carts and their items.")
+    @ApiOperation(value = "List all carts.", notes = "Produces a JSON object containing all registered carts and their items.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Search successful."),
             @ApiResponse(code = 404, message = "Cart not found."),
@@ -50,7 +51,7 @@ public class CartController {
      * @param id ID to match the cart with.
      * @return If the search is positive, a matching cart.
      */
-    @Operation(summary = "Find a cart by ID.", description = "Produces a JSON object with details about the requested cart.")
+    @ApiOperation(value = "Find a cart by ID.", notes = "Produces a JSON object with details about the requested cart.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Search successful."),
             @ApiResponse(code = 404, message = "Cart not found."),
@@ -66,7 +67,7 @@ public class CartController {
      * @param userId Referenced user ID to match the cart with.
      * @return A matching cart, if the search is positive.
      */
-    @Operation(summary = "Find a cart by its user ID", description = "Produce a JSON object with details about the matching cart, verifying beforehand that the user exists by running a search against the Users microservice.")
+    @ApiOperation(value = "Find a cart by its user ID", notes = "Produce a JSON object with details about the matching cart, verifying beforehand that the user exists by running a search against the Users microservice.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Search successful."),
             @ApiResponse(code=404, message = "User or cart not found.")
@@ -81,7 +82,7 @@ public class CartController {
      * @param id ID to match the cart with.
      * @return A matching cart's item set.
      */
-    @Operation(summary = "Show the contents of a car.t", description = "Produce a JSON object with details about a cart's items.")
+    @ApiOperation(value = "Show the contents of a car.t", notes = "Produce a JSON object with details about a cart's items.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Request completed successfully."),
             @ApiResponse(code = 404, message = "Cart not found."),
@@ -100,7 +101,7 @@ public class CartController {
      * @return A copy of the new cart object.
      */
     @PostMapping
-    @Operation(summary = "Create a new cart.", description = "Given a JSON object containing a string named \"description\", create a new cart.")
+    @ApiOperation(value = "Create a new cart.", notes = "Given a JSON object containing a string named \"description\", create a new cart.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Request completed successfully."),
             @ApiResponse(code = 400, message = "Bad request. Try again.")
@@ -118,7 +119,7 @@ public class CartController {
      * @param quantity Quantity of the product to add to the set.
      * @return An updated cart entry.
      */
-    @Operation(summary = "Add an item to a cart.", description = "Given a cart's ID, a product's ID and a quantity, add a new item (associated to a product) to a cart's item set.")
+    @ApiOperation(value = "Add an item to a cart.", notes = "Given a cart's ID, a product's ID and a quantity, add a new item (associated to a product) to a cart's item set.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Element added."),
             @ApiResponse(code = 400, message = "Bad request. Invalid quantity."),
@@ -135,7 +136,7 @@ public class CartController {
      * @param cartId ID of the cart to affect.
      * @param itemSet Item set to add to the cart. Read from JSON.
      */
-    @Operation(summary = "Add multiple items to a cart.", description = "Given a JSON object containing one or more item objects with full information, add those items to a cart's item set.")
+    @ApiOperation(value = "Add multiple items to a cart.", notes = "Given a JSON object containing one or more item objects with full information, add those items to a cart's item set.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Elements added."),
             @ApiResponse(code = 404, message = "Could not find a referenced product from one of the items."),
@@ -155,7 +156,7 @@ public class CartController {
      * @param itemId ID of the item to delete.
      * @return A copy of the updated cart entry.
      */
-    @Operation(summary = "Delete a cart's item.", description = "Given a cart ID and an item ID, remove that item from the cart's item set.")
+    @ApiOperation(value = "Delete a cart's item.", notes = "Given a cart ID and an item ID, remove that item from the cart's item set.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Item removed."),
             @ApiResponse(code = 404, message = "Cart or item not found."),
@@ -170,7 +171,7 @@ public class CartController {
      * Delete a cart.
      * @param cartId ID of the cart to delete.
      */
-    @Operation(summary = "Delete a cart", description = "Given a cart ID, delete a cart and its registered items.")
+    @ApiOperation(value = "Delete a cart", notes = "Given a cart ID, delete a cart and its registered items.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Cart deleted."),
             @ApiResponse(code = 404, message = "Cart not found."),
